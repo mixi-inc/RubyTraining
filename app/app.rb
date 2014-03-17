e108401c515a4e75ec5c4f195bac74f8c19e449d
junsumida <jun.sumida@mixi.co.jp>
2014-03-17 14:00:06 +0900

    integrated todo methods

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require "sinatra/activerecord"
+require 'json'
 
 # require all models
 Dir[File.dirname(__FILE__)+"/model/*.rb"].each {|file| require file }
@@ -11,6 +12,25 @@ end
 
 get '/' do
   'Hello, Moscow!'
+end
+
+get '/todo' do
+  JSON.dump([{
+                 :done => true,
+                 :order => 1,
+                 :title => "done task"
+             },{
+                 :done => false,
+                 :order => 2,
+                 :title => "not done task"
+             }])
+end
+
+post '/todo' do
+  done = params[:done]
+  order = params[:order]
+  title = params[:title]
+  JSON.dump({})
 end
 
 after do
