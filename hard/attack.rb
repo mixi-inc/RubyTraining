5e2e45e11996f2804f53faf82ed3fedb775e5367
NAKANO Ryusuke <ryusuke.nakano@mixi.co.jp>
2014-04-01 17:10:12 +0900

    add hard application

---
+++ hard/attack.rb
@@ -0,0 +1,17 @@
+require 'net/http'
+
+def random_string
+  (("a".."z").to_a + ("A".."Z").to_a + (0..9).to_a).shuffle[0..200].join
+end
+
+10.times{
+  fork do
+    Net::HTTP.start("localhost", 9292){|http|
+      while true
+        http.get("/hoge?#{random_string}=1")
+      end
+    }
+  end
+}
+
+Process.waitall
